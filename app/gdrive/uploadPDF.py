45876f504c7f7939fbe8ee3467c98f4bc39aafb8
zizy97 <supuntharuka9749@gmail.com>
2021-06-03 12:04:43 +0000

    Gdrive upload created

--- app/gdrive/uploadPDF.py
+++ app/gdrive/uploadPDF.py
@@ -5,21 +5,30 @@ drive_service = create_service()
 
 
 def uploadPDF(filepath, filename):
-    file_metadata = {'name': filename, "parents": ['18X9FVtm0L0LrEvfiFXe0KqcJKSz30G_3']
-                     }
-    media = MediaFileUpload(filepath, mimetype='application/pdf', resumable=True)
-    file = drive_service.files().create(body=file_metadata,
-                                        media_body=media,
-                                        ).execute()
+    results = drive_service.files().list(fields="files(id,name)").execute()
+    items = results.get('files', [])
+    dic = {}
+    for item in items:
+        dic.update({item.get('name'): item.get('id')})
 
-    file_id = file.get('id')
-    user_permissions = {
-        'type': 'anyone',
-        'role': 'reader'
-    }
-    response = drive_service.permissions().create(
-        fileId=file_id,
-        body=user_permissions
-    ).execute()
+    if filename not in dic:
+        file_metadata = {'name': filename, "parents": ['18X9FVtm0L0LrEvfiFXe0KqcJKSz30G_3']
+                         }
+        media = MediaFileUpload(filepath, mimetype='application/pdf', resumable=True)
+        file = drive_service.files().create(body=file_metadata,
+                                            media_body=media,
+                                            ).execute()
+
+        file_id = file.get('id')
+        user_permissions = {
+            'type': 'anyone',
+            'role': 'reader'
+        }
+        drive_service.permissions().create(
+            fileId=file_id,
+            body=user_permissions
+        ).execute()
+    else:
+        file_id = dic[filename]
     response = drive_service.files().get(fileId=file_id, fields='webContentLink').execute()
     return response.get('webContentLink')
