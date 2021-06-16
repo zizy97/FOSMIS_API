47f611d2bd05f2ffaea3b0ce4d855e87af303390
zizy97 <supuntharuka9749@gmail.com>
2021-06-16 22:33:34 +0530

    logging added to initial file

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,18 @@
 from app.fosmis import updateDB
+import logging
 import asyncio
 
 
 async def main():
+    count = 0
+    logging.info("Initial Run Stated!!!")
     while True:
+        logging.info(f"round {count} initialized!!!")
         try:
             await asyncio.sleep(60*10)
             updateDB()
         except Exception as e:
-            print(e)
+            logging.info(f"Error Occured - {e}")
 
 if __name__ == "__main__":
     asyncio.run(main())
