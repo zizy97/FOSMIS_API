e2eafe623d6573bc68827895b3ab37f5f5c1d243
zizy97 <supuntharuka9749@gmail.com>
2021-06-17 14:22:48 +0530

    token updated

--- wsgi.py
+++ wsgi.py
@@ -4,17 +4,17 @@ import asyncio
 
 
 async def main():
-    count = 0
-    logging.info("Initial Run Stated!!!")
-    while True:
-        logging.info(f"round {count} initialized!!!")
-        try:
-            await asyncio.sleep(60 * 10)
-            updateDB()
-        except Exception as e:
-            logging.info(f"Error Occured - {e}")
-        count += 1
-
+    # count = 0
+    # logging.info("Initial Run Stated!!!")
+    # while True:
+    #     logging.info(f"round {count} initialized!!!")
+    #     try:
+    #         await asyncio.sleep(60 * 10)
+    #         updateDB()
+    #     except Exception as e:
+    #         logging.info(f"Error Occured - {e}")
+    #     count += 1
+    updateDB()
 
 if __name__ == "__main__":
     asyncio.run(main())
