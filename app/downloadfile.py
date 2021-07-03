cf3f02a92133f052ce0bbb6f41c0bfc7e93a0d1a
zizy97 <56763116+zizy97@users.noreply.github.com>
2021-07-03 23:42:15 +0530

    Update downloadfile.py

--- app/downloadfile.py
+++ app/downloadfile.py
@@ -1,29 +1,29 @@
-import pathlib
-import os
+# import pathlib
+# import os
 
-from .gdrive import uploadPDF
-from . import logging
+# from .gdrive import uploadPDF
+# from . import logging
 
-log = logging.getLogger(__name__)
+# log = logging.getLogger(__name__)
 
-dirName = 'pdf'
-try:
-    # Create target Directory
-    os.mkdir(dirName)
-    print("Directory ", dirName, " Created ")
-except FileExistsError:
-    print("Directory ", dirName, " already exists")
+# dirName = 'pdf'
+# try:
+#     # Create target Directory
+#     os.mkdir(dirName)
+#     print("Directory ", dirName, " Created ")
+# except FileExistsError:
+#     print("Directory ", dirName, " already exists")
 
 
-def download_file(ses, url, filename):
-    path = str(pathlib.Path().absolute()) + f'/pdf/{filename}.pdf'
-    if not os.path.exists(path):
-        log.info("downloading %s ...", filename)
-        response = ses.get(url)
-        file = open(path, 'wb')
-        file.write(response.content)
-        file.close()
-    else:
-        log.info("%s file exist download skipped....", filename)
-    link = uploadPDF(path, filename)
-    return {"path": link}
+# def download_file(ses, url, filename):
+#     path = str(pathlib.Path().absolute()) + f'/pdf/{filename}.pdf'
+#     if not os.path.exists(path):
+#         log.info("downloading %s ...", filename)
+#         response = ses.get(url)
+#         file = open(path, 'wb')
+#         file.write(response.content)
+#         file.close()
+#     else:
+#         log.info("%s file exist download skipped....", filename)
+#     link = uploadPDF(path, filename)
+#     return {"path": link}
