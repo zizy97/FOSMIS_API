0cbdf319552f6e751146f07a2220f44788d6e3ba
zizy97 <56763116+zizy97@users.noreply.github.com>
2021-07-04 02:36:16 +0530

    Update getcontent.py

--- app/getcontent.py
+++ app/getcontent.py
@@ -1,13 +1,13 @@
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
 
 
-# def get_html_content(ses, url):
-#     res = ses.get(url)
-#     content_ = res.content
-#     soup = BeautifulSoup(content_, "lxml")
-#     data = soup.findAll("div", attrs={"id": "m"})
-#     description = []
-#     for des in data:
-#         if des.text:
-#             description.append(des.text)
-#     return "\n".join(description)
+def get_html_content(ses, url):
+    res = ses.get(url)
+    content_ = res.content
+    soup = BeautifulSoup(content_, "lxml")
+    data = soup.findAll("div", attrs={"id": "m"})
+    description = []
+    for des in data:
+        if des.text:
+            description.append(des.text)
+    return "\n".join(description)
