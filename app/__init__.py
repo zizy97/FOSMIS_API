61689458ce64472140a1088e38b66673eacde9d6
zizy97 <56763116+zizy97@users.noreply.github.com>
2021-07-04 02:37:31 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from .logging import logging
-# from .getcontent import get_html_content
+from .getcontent import get_html_content
 # from .downloadfile import download_file
 from .fosmis import updateDB
 
